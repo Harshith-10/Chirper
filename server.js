3621e0c7f1cb7be7962599d3b7ce4d971f17a6e5
Harshith <hdr8712865491@gmail.com>
2025-04-23 12:42:32 +0530

    Add Multiple Peer Support
    Added support for multiple peers by registering with a username

--- server.js
+++ server.js
@@ -1,26 +1,56 @@
 const { Server } = require("socket.io");
 const http = require("http");
+const express = require("express");
 
-const server = http.createServer();
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*", // In production, specify your frontend's origin
-  },
+  cors: { origin: "*" }
 });
 
+const users = {}; // username -> socket.id
+
 io.on("connection", (socket) => {
+  let username = null;
+
   console.log(`New client connected: ${socket.id}`);
 
   // Send a welcome message to the newly connected client
   socket.emit("server-message", "✅ Signaling server is up and running!");
 
-  // Relay signaling data to other clients
-  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
-  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
-  socket.on("ice", (data) => socket.broadcast.emit("ice", data));
+  socket.on("register", (name) => {
+    username = name;
+    users[username] = socket.id;
+    io.emit("user-list", Object.keys(users));
+  });
+
+  socket.on("offer", ({ to, offer }) => {
+    if (users[to]) {
+      io.to(users[to]).emit("offer", { from: username, offer });
+    }
+  });
+
+  socket.on("answer", ({ to, answer }) => {
+    if (users[to]) {
+      io.to(users[to]).emit("answer", { from: username, answer });
+    }
+  });
+
+  socket.on("ice", ({ to, candidate }) => {
+    if (users[to]) {
+      io.to(users[to]).emit("ice", { from: username, candidate });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    if (username) {
+      delete users[username];
+      io.emit("user-disconnected", username);
+      io.emit("user-list", Object.keys(users));
+    }
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Signaling server running on http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log("Signaling server running on http://localhost:3000");
 });
